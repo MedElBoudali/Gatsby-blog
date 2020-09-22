7afa1aa4b123d91a3f4019eab664b4424604ccde
MedEBoudali <med.sneaky@gmail.com>
2020-09-22 22:37:47 +0000

    add blog post template to generate posts pages

---
+++ src/templates/blog-post.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>{post.frontmatter.title}</div>
+      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: string!) {
+    markdonRemark(fields: { slug: { eq: $slug } })
+  }
+`
