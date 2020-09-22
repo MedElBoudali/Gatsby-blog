59092bbabf172fb9982a02b1fee6e627c98198ed
MedEBoudali <med.sneaky@gmail.com>
2020-09-22 14:28:39 +0000

    styling blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 export default ({ data }) => (
@@ -19,24 +18,52 @@ export default ({ data }) => (
         This is my 1st Gatsby blog.
       </h1>
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h3 style={{ color: "#595959", fontSize: "1rem", textTransform: 'uppercase' }}>
-            {node.frontmatter.title} -
-            <span
+        <div style={{ margin: "10px 0 50px 0" }} key={node.id}>
+          <h3
+            style={{
+              color: "#595959",
+              fontSize: "1rem",
+              textTransform: "uppercase",
+              margin: "0",
+            }}
+          >
+            {node.frontmatter.title}
+          </h3>
+          <span
+            style={{
+              fontSize: "0.8rem",
+              color: "darkGray",
+              margin: "0 0 10px 0",
+            }}
+          >
+            {node.frontmatter.date}-
+            <svg
+              viewBox="0 0 24 24"
               style={{
-                marginLeft: "5px",
-                fontSize: "0.8rem",
-                color: "darkGray",
+                stroke: "none",
+                width: "20px",
+                height: "20px",
+                fill: "rgb(209, 209, 209)",
               }}
             >
-              {node.frontmatter.date}
-            </span>
-          </h3>
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="12" cy="12" r="9" />
+              <polyline points="12 7 12 12 15 15" />
+            </svg>
+            {node.timeToRead}min.
+          </span>
+          <div
+            style={{
+              width: "100%",
+              height: "150px",
+              backgroundImage: `url(${node.frontmatter.thumbnail})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
         </div>
       ))}
-      <h7>
-        Reading time: {data.allMarkdownRemark.edges[0].node.timeToRead}min.
-      </h7>
       <h6>Total blogs: {data.allMarkdownRemark.totalCount}</h6>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
@@ -52,6 +79,7 @@ export const query = graphql`
           frontmatter {
             title
             description
+            thumbnail
             date
           }
           html
