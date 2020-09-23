72b6e80c28307ba895b90b0dacf4897729b680e5
MedEBoudali <med.sneaky@gmail.com>
2020-09-23 12:41:44 +0000

    add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,101 +1,95 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const Section = styled(section)`
+  width: 100%;
+  min-height: 100%;
+  text-align: left;
+`
+const BlogContainer = styled(div)`
+  margin: 10px 0 50px 0;
+`
+const BlogTitle = styled(Link)`
+  color: #595959;
+  width: fit-content;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 0;
+  text-decoration: none;
+  display: block;
+`
+
+const BlogSubtitle = styled(span)`
+  font-size: 0.8rem;
+  color: darkGray;
+  margin: 0 0 10px 0;
+  display: inline-flex;
+  align-items: center;
+  vertical-align: middle;
+`
+
+const Svg = styled(svg)`
+  margin: 0 3px 0 10px;
+  fill: none;
+  width: 15px;
+  height: 15px;
+  stroke: darkGray;
+  stroke-width: 1.5;
+  stroke-linecap: round;
+`
+
+const Thumbnail = styled(div)`
+  margin: 0 2%;
+  width: 96%;
+  height: 250px;
+  background-image: ${({url}) => `url(${url})`};
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`
+
+const Excerpt = styled(p)`
+  margin: 0 2%;
+  width: 96%;
+  font-size: 0.7rem;
+  font-weight: bold;
+  text-align: justify;
+  text-justify: inter-word;
+`
 
 export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <section
-      style={{
-        width: "100%",
-        minHeight: "100%",
-        textAlign: "left",
-      }}
-    >
+    <Section>
       {/* Posts */}
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div style={{ margin: "10px 0 50px 0" }} key={node.id}>
+        <BlogContainer key={node.id}>
           {/* Title */}
-          <a
-            style={{
-              color: "#595959",
-              width: "fit-content",
-              fontSize: "1rem",
-              fontWeight: "bold",
-              textTransform: "uppercase",
-              margin: "0",
-              textDecoration: "none",
-              display: "block",
-            }}
-            href={node.fields.slug}
-            // target="_blank"
-            // rel="noreferrer"
-          >
-            {node.frontmatter.title}
-          </a>
+          <BlogTitle to={node.fields.slug}>{node.frontmatter.title}</BlogTitle>
           {/* Data & time to read */}
-          <span
-            style={{
-              fontSize: "0.8rem",
-              color: "darkGray",
-              margin: "0 0 10px 0",
-              display: "inline-flex",
-              alignItems: "center",
-              verticalAlign: "middle",
-            }}
-          >
+          <BlogSubtitle>
             {node.frontmatter.date}
-            <svg
-              viewBox="0 0 24 24"
-              style={{
-                margin: "0 3px 0 10px",
-                fill: "none",
-                width: "15px",
-                height: "15px",
-                stroke: "darkGray",
-                strokeWidth: "1.5",
-                strokeLinecap: "round",
-              }}
-            >
+            <Svg viewBox="0 0 24 24">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <circle cx="12" cy="12" r="9" />
               <polyline points="12 7 12 12 15 15" />
-            </svg>
+            </Svg>
             {node.timeToRead} min.
-          </span>
+          </BlogSubtitle>
           {/* Thumbnail */}
-          <div
-            style={{
-              margin: "0 2%",
-              width: "96%",
-              height: "250px",
-              backgroundImage: `url(${node.frontmatter.thumbnail})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          />
+          <Thumbnail url={node.frontmatter.thumbnail} />
           {/* Excerpt */}
-          <p
-            style={{
-              margin: "0 2%",
-              width: "96%",
-              fontSize: "0.7rem",
-              fontWeight: "bold",
-              textAlign: "justify",
-              textJustify: "inter-word",
-            }}
-          >
-            {node.excerpt}
-          </p>
-        </div>
+          <Excerpt>{node.excerpt}</Excerpt>
+        </BlogContainer>
       ))}
-      <h6>Total blogs: {data.allMarkdownRemark.totalCount}</h6>
+      {/* <h6>Total blogs: {data.allMarkdownRemark.totalCount}</h6>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </section>
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+    </Section>
   </Layout>
 )
 
